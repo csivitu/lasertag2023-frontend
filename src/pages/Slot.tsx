68d7ca855f871f88b1a6212e313e53863cbd8166
Arv2040 <aravcr4085@gmail.com>
2023-09-02 21:25:52 +0530

    small changes

--- src/pages/Slot.tsx
+++ src/pages/Slot.tsx
@@ -17,7 +17,7 @@ export default function Slot() {
                   <div className="relative flex flex-col items-center">
                     <Image src="/landingassests/Red.svg" alt="button" width={100} height={100} className="w-11/12 h-11/12 relative" />
                     <Image src="/landingassests/button.svg" alt="button" width={300} height={300} className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-3/4" />
-                    <p className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full text-white ${tektur.className} font-medium text-2xl font-bold text-center mt-32">
+                    <p className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full text-white ${tektur.className} text-2xl font-bold text-center mt-32">
                       You have a limited time to join the <br />cyber-space and save us all.
                     </p>
                   </div>
