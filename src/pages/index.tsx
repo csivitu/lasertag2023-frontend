561bb88223fd0b038060636f76754b8b7a3b5a29
Vaibhavi <135148044+Vaibhavi028@users.noreply.github.com>
2023-09-02 13:17:55 +0530

    Done with 4th page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Landing from "./Landing";
 import Guide from "./Guide";
+import FAQ from './faq';
+//import Slot from "./Slot";
 
 export default function Home() {
   return (
     <>
       <Landing />
       <Guide />
+      <FAQ/>
     </>
   );
 }
