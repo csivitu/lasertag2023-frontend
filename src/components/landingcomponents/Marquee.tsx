72856764251ed732e0fb1c968cb5738b681ede52
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-19 01:25:12 +0530

    fix: slider fix

--- src/components/landingcomponents/Marquee.tsx
+++ src/components/landingcomponents/Marquee.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import 'tailwindcss/tailwind.css'
-import Image from 'next/image'
-import { Tektur } from 'next/font/google'
+import React from "react";
+import "tailwindcss/tailwind.css";
+import Image from "next/image";
+import { Tektur } from "next/font/google";
 import Marquee from "react-fast-marquee";
 
-const tektur = Tektur({subsets:['latin']})
+const tektur = Tektur({ subsets: ["latin"] });
 
 export default function Footer() {
   return (
-   
-    
-      
-            <main className  ={`relative ${tektur.className} flex justify-center items-center bg-footerImg2 bg-contain h-[120px]  `} >
-            {/* <Image src = "/landingassests/Vector.svg" alt = "footervector" width = {194} height = {194} className = "w-full "/> */}
-           <Marquee autoFill = {true} speed = {30} className = " absolute top-6 overflow-hidden h-fit font-medium text-white text-3xl"><p className = " text-black mx-6">ARE YOU READY? </p><p className = "text-black mx-6">TO FIGHT FOR YOUR TEAM</p></Marquee>
-           </main>
-        
-
-
-
-
-
-    
-  )
+    <main
+      className={`relative ${tektur.className} flex justify-center items-center bg-footerImg2 bg-contain h-[120px]  `}
+    >
+      {/* <Image src = "/landingassests/Vector.svg" alt = "footervector" width = {194} height = {194} className = "w-full "/> */}
+      <Marquee
+        autoFill={true}
+        speed={30}
+        className=" absolute top-6 overflow-hidden h-fit font-medium text-white text-3xl"
+      >
+        <p className=" text-black mx-6">ARE YOU READY? </p>
+        <p className="text-black mx-6">TO FIGHT FOR YOUR TEAM</p>
+      </Marquee>
+    </main>
+  );
 }
