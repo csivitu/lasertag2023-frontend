a73ef17a409ccf612d32bc4444719e17d79bf6c3
anubhav-aryan <aryananubhav08@gmail.com>
2023-09-19 20:04:54 +0530

    fix: caraousel

--- src/components/landingcomponents/Photo.tsx
+++ src/components/landingcomponents/Photo.tsx
@@ -24,59 +24,75 @@ const Slideshow: React.FC = () => {
   ]; // Define your image paths here
 
   return (
-    <div className=""id="gallery">
-      <div className="container mx-auto relative w-full" data-carousel="static">
-        <div className="relative overflow-hidden rounded-lg">
-          {imagePaths.map((path, index) => (
-            <div
-              key={index}
-              className={`duration-700 ease-in-out ${
-                index === activeIndex ? "block" : "hidden"
-              }`}
-              data-carousel-item={index === activeIndex ? "active" : undefined}
-            >
-              <Image
-                src={path} // Use the image path from the array
-                alt={`Image ${index + 1}`}
-                width={750} // Adjust the width and height as needed
-                height={750}
-              />
-            </div>
-          ))}
-        </div>
-
-        <button
-          type="button"
-          className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev
-          onClick={handlePrevClick}
-        >
-          <Image
-            src="/landingassests/carouselassets/goleft.svg"
-            alt="Left arrow"
-            width={40}
-            height={40}
-          />
-
-          {/* Left arrow icon */}
-        </button>
-
-        <button
-          type="button"
-          className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next
-          onClick={handleNextClick}
-        >
-          <Image
-            src="/landingassests/carouselassets/goright.svg"
-            alt="Left arrow"
-            width={40}
-            height={40}
-          />{" "}
-          {/* Right arrow icon */}
-        </button>
+    <>
+      <div className="text-white font-bold text-6xl">
+        A Thrilling Multiplayer Shooting Experience{" "}
       </div>
-    </div>
+      <div className="text-white font-medium text-2xl">
+        Laser Tag offers you a chance to step onto the arena for a thrilling
+        multi-player experience. <br />
+        Don your vests and have your guns at the ready, maximize hits to climb
+        up the leaderboard!
+      </div>
+      <div className="" id="gallery">
+        <div
+          className="container mx-auto relative w-full"
+          data-carousel="static"
+        >
+          <div className="relative overflow-hidden rounded-lg">
+            {imagePaths.map((path, index) => (
+              <div
+                key={index}
+                className={`duration-700 ease-in-out ${
+                  index === activeIndex ? "block" : "hidden"
+                }`}
+                data-carousel-item={
+                  index === activeIndex ? "active" : undefined
+                }
+              >
+                <Image
+                  src={path} // Use the image path from the array
+                  alt={`Image ${index + 1}`}
+                  width={750} // Adjust the width and height as needed
+                  height={750}
+                />
+              </div>
+            ))}
+          </div>
+
+          <button
+            type="button"
+            className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-prev
+            onClick={handlePrevClick}
+          >
+            <Image
+              src="/landingassests/carouselassets/goleft.svg"
+              alt="Left arrow"
+              width={40}
+              height={40}
+            />
+
+            {/* Left arrow icon */}
+          </button>
+
+          <button
+            type="button"
+            className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next
+            onClick={handleNextClick}
+          >
+            <Image
+              src="/landingassests/carouselassets/goright.svg"
+              alt="Left arrow"
+              width={40}
+              height={40}
+            />{" "}
+            {/* Right arrow icon */}
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
